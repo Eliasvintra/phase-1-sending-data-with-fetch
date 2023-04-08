dc414a75f1514b8933dae85c968de40943c787f5
Eliasvintra <rbnvintra.ke@gmail.com>
2023-04-08 14:46:20 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+// // Add your code here
+
+function submitData(name, email){
+    
+    return fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            Accept: 'application/json',
+            },
+            body: JSON.stringify({
+                name,
+                email
+            }),
+        })
+        .then( res => res.json())
+        .then( data => {
+            const new_id = document.createElement('p');
+            new_id.textContent = data.id;
+            document.querySelector('body').appendChild(new_id);
+        
+        })
+        .catch( function ( error ) {      
+            document.body.innerHTML = error.message    
+        
+        } )}
+    
+ submitData("Steve", "steve@steve.com");
